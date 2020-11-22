a1bf23252f9de347f67e3ecb4f7761031c343ad2
Murshidmac <mh.murshidco.uk@outlook.com>
2020-11-22 12:33:53 +0530

    authentication done with developer experience

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,13 +2,47 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
-const validateRegisterInput = require('../../util/validators');
+const { validateRegisterInput, validateLoginInput } = require('../../util/validators');
 const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
 
 
+function generateToken(user){
+    return jwt.sign({
+        id: user.id,
+        email: user.email,
+        username: user.username
+    }, SECRET_KEY, { expiresIn: '1h' });
+}
+
+
 module.exports = {
     Mutation: {
+        async login(_, { email, password}){
+            const { errors, valid } = validateLoginInput(email, password);
+
+            if(!valid){
+                throw new UserInputError('Username or Email is not valid',{ errors });
+            }
+
+            const user = await User.findOne({email});
+            if(!user){
+                errors.general = 'Email not found';
+                throw new UserInputError('Username or Email does not exist',{ errors });
+            }
+            const match = await bcrypt.compare(password, user.password);
+            if(!match){
+                errors.general = 'Wrong credentials';
+                throw new UserInputError('Wrong credentials',{ errors });    
+            }
+
+            const token = generateToken(user)
+            return {
+                ...user._doc,
+                id: user._id,
+                token
+            }
+        },
         async register(_,
             { 
                 registerInput: { username, email, password, confirmpassword
@@ -41,11 +75,7 @@ module.exports = {
             });
 
             const res = await newUser.save()
-            const token = jwt.sign({
-                id: res.id,
-                email: res.email,
-                username: res.username
-            }, SECRET_KEY, { expiresIn: '1h' });
+            const token = generateToken(res)
 
             return {
                 ...res._doc,
--- util/validators.js
+++ util/validators.js
@@ -5,26 +5,45 @@ module.exports.validateRegisterInput = (
     confirmpassword
 ) => {
     const errors = {};
-    if(username.trim() === ''){
+    if (username.trim() === '') {
         errors.username = 'Username must not be empty';
     }
-    if(password === ''){
+    if (password === '') {
         errors.username = 'Password must not be empty';
-    }else if(password !== confirmpassword){
-        errors.confirmpassword = 'Passwords must match' 
+    } else if (password !== confirmpassword) {
+        errors.confirmpassword = 'Passwords must match'
     }
-    if(email.trim() === ''){
+    if (email.trim() === '') {
         errors.username = 'Email must not be empty';
-    }else{
+    } else {
         const regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        if(!email.match(regEx)){
+        if (!email.match(regEx)) {
             errors.email = 'Email must be a valid address'
         }
     }
-    if(confirmpassword === ''){
-        errors.username = 'Confirm must not be empty';
+    if (confirmpassword === '') {
+        errors.confirmpassword = 'Confirm password must not be empty';
     }
 
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    }
+}
+
+module.exports.validateLoginInput = (email, password) => {
+    const errors = {};
+    if (email.trim() === '') {
+        errors.username = 'Email must not be empty';
+    } else {
+        const regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (!email.match(regEx)) {
+            errors.email = 'Email must be a valid address'
+        }
+    }
+    if (password.trim() === '') {
+        errors.password = 'Password must not be empty';
+    }
     return {
         errors,
         valid: Object.keys(errors).length < 1
